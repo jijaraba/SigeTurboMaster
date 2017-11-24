b4ece4200809ebe3e8a128860deb50d6f639d164
José Jaraba <jijaraba@hotmail.com>
2017-11-23 23:56:39 -0500

    Cambios menores

---
+++ config/report.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'username' => env('JASPERSERVER_USERNAME', 'jasperadmin'),
+    'password' => env('JASPERSERVER_PASSWORD'),
+    'version' => env('JASPERSERVER_VERSION', 'rest_v2')
+];
