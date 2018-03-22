d8b98503783f10d617384f6481534449e81a273d
José Ignacio Jaraba Atehortúa <jijaraba@hotmail.com>
2018-03-22 13:24:14 -0500

    Cambios en Módulo Director de Grupo

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -307,7 +307,7 @@ class PaymentsController extends Controller
      */
     public function getPaymentsPendingsByUser(Request $request)
     {
-        return response()->json($this->paymentRepository->getPaymentsPendingsByUser($request['user'],true));
+        return response()->json($this->paymentRepository->getPaymentsPendingsByUser($request['user'],true, null, 'ASC', true));
     }
 
     /**
